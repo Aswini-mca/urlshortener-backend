8e52f738c23513f625524c5936c4349a7f406482
Aswini-mca <aswini25.mca@gmail.com>
2023-10-16 16:14:34 +0530

    updated mongo url

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(express.json())
 app.use(cors())
 
 //mongoDB connection
-const MONGO_URL = "mongodb://127.0.0.1:27017"
-// process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL
 
 async function createConnection() {
     const client = new MongoClient(MONGO_URL)
